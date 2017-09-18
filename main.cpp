70a14e57c444875b62eb11bbe90e0102bfec9daa
Tomas Landberg <tomas.landberg@gmail.com>
2017-09-18 02:29:42 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <algorithm>
+#include <vector>
+#include "pb.h"
+
+int main(void)
+{
+    Game Game1;
+    Game1.Menu();
+    Game1.Play();
+    return 0;
+}
