ba1d76aeac0fb7a00fd649b1a9c7a536b95f5be0
Tomas Landberg <tomas.landberg@gmail.com>
2020-11-26 22:14:56 +0100

    Changed to cmake, and compiles with warning flags now. This time with files.

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/**
+ *  Poweball, a simple game that generates random numbers
+ *  Copyright (C) 2017  Tomas Landberg
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <algorithm>
+#include <vector>
+#include "pb.h"
+
+int main(void)
+{
+    Game Game1;
+    Game1.Menu();
+    Game1.Play();
+    return 0;
+}
